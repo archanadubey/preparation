33b56817b1d536c33bacac3212ed5ccdf338426c
Archana <archooworship@gmail.com>
2024-10-19 00:08:07 -0700

    array literal initail commit

---
+++ src/PatternLoops/ArrayExercise.java
@@ -0,0 +1,10 @@
+package PatternLoops;
+
+public class ArrayExercise {
+    public static void arrayLiteral(){
+        int [] intArray  = {1,2,3,4,5,6,7};
+        for(int i = 0; i< intArray.length;i++){
+            System.out.println( "index of arrays are : "+i+" "+intArray[i]);
+        }
+    }
+}
