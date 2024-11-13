00bce9849d4d80480c621f91a00f74feb7c26d4d
Archana <archooworship@gmail.com>
2024-11-12 18:01:46 -0800

    basic String

---
+++ src/StringLearning/BasicStringDemo.java
@@ -0,0 +1,17 @@
+package StringLearning;
+
+public class BasicStringDemo {
+    public static void main(String[] args) {
+
+
+        // creating string by using newkeyword
+        String str = new String("Example");
+
+        // It will create two object in the memory
+        //First it will create an object in heap are
+        //then it will create an abject in String constant pool
+
+
+        System.out.println(str);
+    }
+}
