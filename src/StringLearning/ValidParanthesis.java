d83c5ac99d7b6bbb176f6bdfe584d250b7453bb7
Archana <archooworship@gmail.com>
2024-12-08 23:56:48 -0800

    ValidParanthesis

---
+++ src/StringLearning/ValidParanthesis.java
@@ -0,0 +1,41 @@
+package StringLearning;
+
+public class ValidParanthesis {
+
+    public  boolean isValidParanthesis(String s){
+
+        // get the length of s
+
+      int   length = s.length();
+      do {
+          s = s.replace("()", "").replace("{}", "").replace("[]", "");
+
+
+      }
+        while ( length != s.length());
+
+//            if (s.isEmpty()){
+//
+//                return true;
+//            }
+//        return false;
+
+            return s.length() == 0;
+        }
+
+
+    public static void main(String[] args) {
+
+        String str = "({[]})]";
+
+        ValidParanthesis vp = new ValidParanthesis();
+      boolean result =  vp.isValidParanthesis(str);
+        System.out.println(result);
+
+     //   boolean validParanthesis = isValidParanthesis(str);
+
+
+    }
+
+}
+//}
