065d20d62a46fde7356fc0bbed12f319b7e3dfe0
Archana <archooworship@gmail.com>
2025-01-02 23:58:19 -0800

    findLargestGoodInteger leetcode  Array committed

--- src/StringLearning/LargestGoodInteger.java
+++ src/StringLearning/LargestGoodInteger.java
@@ -1,5 +1,7 @@
 package StringLearning;
 
+import java.util.Stack;
+
 public class LargestGoodInteger {
 
     private static String findLargestGoodInteger(String num) {
@@ -22,10 +24,32 @@ public class LargestGoodInteger {
     public static void main(String[] args) {
 
         System.out.println(findLargestGoodInteger("1233331"));
-        System.out.println(findLargestGoodInteger("12"));
+       // System.out.println(findLargestGoodInteger("12"));
         System.out.println(findLargestGoodInteger("13000654"));
-
+        String s1 = "leetcode";
+        System.out.println(findLexicographicallySmallestString(s1));
 
     }
+
+    private static String findLexicographicallySmallestString(String sequence) {
+        int n = sequence.length(); // Length of the input sequence
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 1; i <= n + 1; i++) {
+            // Push the current number onto the stack
+            stack.push(i);
+
+            // Process the stack when we're at the last number or when sequence[i - 1] == 'A'
+            if (i == n + 1 || (i <= n && sequence.charAt(i - 1) == 'A')) {
+                while (!stack.isEmpty()) {
+                    result.append(stack.pop());
+                }
+            }
+        }
+
+        return result.toString();
+    }
+
 
 }
